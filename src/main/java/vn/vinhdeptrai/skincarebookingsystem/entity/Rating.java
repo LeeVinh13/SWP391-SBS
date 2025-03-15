5c3987ab90e83107784c581809e0ab63cf609243
VietAnh <anhviettran357@gmail.com>
2025-03-16 00:18:26 +0700

    rating entity (one to one with appointment) - can feedback when rating or let it null

---
+++ src/main/java/vn/vinhdeptrai/skincarebookingsystem/entity/Rating.java
@@ -0,0 +1,35 @@
+package vn.vinhdeptrai.skincarebookingsystem.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "ratings")
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @OneToOne
+    @JoinColumn(name = "appointment_id", nullable = false)
+    private Appointment appointment;//thuộc appointment nào, để check đã trải nghiệm chưa, đã feedback chưa
+
+    private int stars;
+
+    private String comment;
+
+    private LocalDateTime createdAt;
+    
+    private LocalDateTime updatedAt;
+}
