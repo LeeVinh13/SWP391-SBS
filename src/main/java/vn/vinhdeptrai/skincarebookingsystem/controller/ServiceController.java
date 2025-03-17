d687ccc2586d2637de1101fcb5d3857979fb9725
VietAnh <anhviettran357@gmail.com>
2025-03-18 00:47:18 +0700

    service pagination

--- src/main/java/vn/vinhdeptrai/skincarebookingsystem/controller/ServiceController.java
+++ src/main/java/vn/vinhdeptrai/skincarebookingsystem/controller/ServiceController.java
@@ -3,6 +3,8 @@ package vn.vinhdeptrai.skincarebookingsystem.controller;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -61,5 +63,28 @@ public class ServiceController {
         return ApiResponse.<ServiceResponse>builder()
                 .build();
     }
+
+    @GetMapping("/sort/{field}/{sortOrder}")
+    public ApiResponse<List<ServiceResponse>> getAllWithSort(@PathVariable String field, @PathVariable String sortOrder) {
+        return ApiResponse.<List<ServiceResponse>>builder()
+                .result(service.getAllWithSort(field, sortOrder))
+                .build();
+    }
+
+    @GetMapping("/pagination/{page}/{size}")
+    public ApiResponse<Page<ServiceResponse>> getAllWithPagination(@PathVariable int page, @PathVariable int size) {
+        return ApiResponse.<Page<ServiceResponse>>builder()
+                .result(service.getAllWithPagination(page, size))
+                .build();
+    }
+
+    @GetMapping("/pagination/{page}/{size}/{field}/{sortOrder}")
+    public ApiResponse<Page<ServiceResponse>> getAllWithPaginationAndSort(@PathVariable int page, @PathVariable int size, @PathVariable String field, @PathVariable String sortOrder) {
+        return ApiResponse.<Page<ServiceResponse>>builder()
+                .result(service.getAllWithPaginationAndSort(page, size, field, sortOrder))
+                .build();
+    }
+    
+    
 }
 
