8e132577b9f6b5b1c62ad08a9bcbb2d5feb07c71
VietAnh <anhviettran357@gmail.com>
2025-03-21 10:17:33 +0700

    fix vn pay call back

--- src/main/java/vn/vinhdeptrai/skincarebookingsystem/controller/PaymentController.java
+++ src/main/java/vn/vinhdeptrai/skincarebookingsystem/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package vn.vinhdeptrai.skincarebookingsystem.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -9,6 +10,8 @@ import vn.vinhdeptrai.skincarebookingsystem.dto.request.PaymentRequest;
 import vn.vinhdeptrai.skincarebookingsystem.dto.response.ApiResponse;
 import vn.vinhdeptrai.skincarebookingsystem.dto.response.PaymentResponse;
 import vn.vinhdeptrai.skincarebookingsystem.service.PaymentService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/payment")
@@ -23,9 +26,10 @@ public class PaymentController {
                 .build();
     }
     @GetMapping("/vn-pay-callback")
-    public ApiResponse<PaymentResponse> payCallbackHandler(HttpServletRequest request) {
-        return ApiResponse.<PaymentResponse>builder()
-                .result(paymentService.handlerVNPayCallback(request))
-                .build();
+    public void payCallbackHandler(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PaymentResponse paymentResponse = paymentService.handlerVNPayCallback(request);
+        if (paymentResponse.getRedirectUrl() != null) {
+            response.sendRedirect(paymentResponse.getRedirectUrl());
+        }
     }
 }
