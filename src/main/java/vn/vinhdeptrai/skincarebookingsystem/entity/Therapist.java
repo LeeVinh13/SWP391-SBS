baebf21aa3ea00ce26d41a25107edf31650c1a17
Vinh Le <vinhpq.dt.468@gmail.com>
2025-03-28 02:09:02 +0700

    update swap role therapist user

--- src/main/java/vn/vinhdeptrai/skincarebookingsystem/entity/Therapist.java
+++ src/main/java/vn/vinhdeptrai/skincarebookingsystem/entity/Therapist.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.OptimisticLockType;
+import org.hibernate.annotations.OptimisticLocking;
 
 import java.util.List;
 
