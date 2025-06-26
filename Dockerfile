8d761fa2e84cbf0958c0e3fcbde0af5e0329ea5b
Vinh Le <vinhpq.dt.468@gmail.com>
2025-06-26 23:14:37 +0700

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@
 #maven:3.8.5-openjdk-17-slim: là image Maven đã cài sẵn Maven 3.8.5 + JDK17, phiên bản slim
 FROM maven:3.9.8-amazoncorretto-21 AS build
 WORKDIR /app
-COPY . ./SWP391-SBS
-WORKDIR /app/SWP391-SBS
-RUN mvn clean package -DskipTests
+COPY . .
+RUN mvn clean package -DskipTests && rm -rf /root/.m2/repository
+#COPY . ./SWP391-SBS
+#WORKDIR /app/SWP391-SBS
+#RUN mvn clean package -DskipTests
 
 #Stage 2: use OpenJDK for running
 FROM  amazoncorretto:21.0.4
--- src/main/java/vn/vinhdeptrai/skincarebookingsystem/SkincareBookingSystemApplication.java
+++ src/main/java/vn/vinhdeptrai/skincarebookingsystem/SkincareBookingSystemApplication.java
@@ -1,10 +1,17 @@
 package vn.vinhdeptrai.skincarebookingsystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "SWP API",version = "1.0",description = "Information"))
+@SecurityScheme(name = "api",scheme = "bearer",type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @EnableScheduling
 public class SkincareBookingSystemApplication {
 
